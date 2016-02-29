49854e2c01842cfa0ff2021925e24ad27940ad41
Daniel Cellucci <dwc238@cornell.edu>
2016-02-28 21:41:41 -0800

    finished population definition

--- 111_lattice_gen.py
+++ 111_lattice_gen.py
@@ -50,83 +50,118 @@ hex_base_height = np.sqrt(3)
 # hexagonal volume, a 30 and 60 degree rotation about the [0,0,1] axis
 
 # Transform matrix that rotates around the [0,0,1] axis by 30 degrees
-init_hex_rot_tform = 0.5*np.array([[np.sqrt(3),        -1,0],
-						           [         1,np.sqrt(3),0],
-						           [         0,         0,2]])
+init_hex_rot_tform = 0.5*np.array([[np.sqrt(3),        -1 ,0],
+						           [        1 , np.sqrt(3),0],
+						           [        0 ,         0 ,2]])
 
 # Transform matrix that rotates around the [0,0,1] axis by 60 degrees
-hex_rot_tform = 0.5*np.array([[         1,-np.sqrt(3),0],
-						      [np.sqrt(3),          1,0],
-						      [         0,          0,2]])
+hex_rot_tform = 0.5*np.array([[        1 ,-np.sqrt(3),0],
+	 						       [np.sqrt(3),         1 ,0],
+	 						       [        0 ,         0 ,2]])
 
+def cubic_to_111(hex_radius,hex_height,cubic_nodes,cubic_frames):
+	extents = np.max([hex_radius,hex_height])
+	mat_matrix = np.zeros((extents*4+4,extents*4+4,extents*4+4))
+	#
+	#creation of the hex volume wireframe
+	#
 
-hex_radius = 3
-hex_height = 3
+	#first node
+	hexnode1 = np.array([hex_radius*hex_triangle_width,0,0])
 
+	hex_nodes = np.array([hexnode1])
 
+	#Applying 60 degree rotation to get the other base nodes
+	for i in range(5):
+		hexnode1 = np.dot(hex_rot_tform,hexnode1)
+		hex_nodes = np.append(hex_nodes,[np.copy(hexnode1)],axis=0)
 
+	# Translating base nodes in the (001) direction to get top nodes
+	hex_nodes = np.append(hex_nodes,hex_nodes+np.array([0,0,hex_base_height*hex_height]),axis=0)
 
-#
-#creation of the hex volume wireframe
-#
-#first node
-hexnode1 = np.array([hex_radius*hex_triangle_width,0,0])
-
-hex_nodes = np.array([hexnode1])
-
-for i in range(5):
-	hexnode1 = np.dot(hex_rot_tform,hexnode1)
-	hex_nodes = np.append(hex_nodes,[np.copy(hexnode1)],axis=0)
-
-hex_nodes = np.append(hex_nodes,hex_nodes+np.array([0,0,hex_base_height*hex_height]),axis=0)
-
-hex_frames = [[ 0, 1],
-			  [ 0, 6],
-			  [ 1, 2],
-			  [ 1, 7],
-			  [ 2, 3],
-			  [ 2, 8],
-			  [ 3, 4],
-			  [ 3, 9],
-			  [ 4, 5],
-			  [ 4,10],
-			  [ 5, 0],
-			  [ 5,11],
-			  [ 6, 7],
-			  [ 7, 8],
-			  [ 8, 9],
-			  [10,11],
-			  [11, 6]]
-
-
-
-cube = [[0.0,0.0,0.0], #0
-		[1.0,0.0,0.0], #1
-		[0.0,1.0,0.0], #2
-		[0.0,0.0,1.0], #3
-		[1.0,1.0,0.0], #4
-		[0.0,1.0,1.0], #5
-		[1.0,0.0,1.0], #6
-		[1.0,1.0,1.0]] #7
-
-frames = [[0,1],
-		  [0,2],
-		  [0,3],
-		  [1,4],
-		  [1,6],
-		  [2,4],
-		  [2,5],
-		  [3,5],
-		  [3,6],
-		  [4,7],
-		  [5,7],
-		  [6,7]]
+	# Frames are defined as relationships between nodes
+	# A visual aid to just help with seeing the volume as a wireframe
+	hex_frames = [[ 0, 1],
+				  [ 0, 6],
+				  [ 1, 2],
+				  [ 1, 7],
+				  [ 2, 3],
+				  [ 2, 8],
+				  [ 3, 4],
+				  [ 3, 9],
+				  [ 4, 5],
+				  [ 4,10],
+				  [ 5, 0],
+				  [ 5,11],
+				  [ 6, 7],
+				  [ 7, 8],
+				  [ 8, 9],
+				  [9,10],
+				  [10,11],
+				  [11, 6]]
 
 
 
+	tot_cube_nodes = np.transpose(np.dot(tform111,np.copy(cubic_nodes).T))
+	tot_cube_frames = np.copy(cubic_frames)
 
+	temptformframe = np.copy(cubic_frames)
 
-nodes = np.transpose(np.dot(tform111,np.transpose(cube)))
+	for delx in range(-2*extents-1,2*extents+1):
+		for dely in range(-2*extents-1,2*extents+1):
+			for delz in range(-2*extents-1,2*extents+1):
+				tformcube = cubic_nodes+np.array([delx,dely,delz])-np.array([0.25,0.25,0.25])
+				tformcube = np.transpose(np.dot(tform111,np.transpose(tformcube)))
+				if in_hex_volume(np.copy(tformcube),hex_radius*hex_triangle_width,hex_height*hex_base_height):
+					tot_cube_nodes = np.append(tot_cube_nodes,tformcube,axis=0)
+					temptformframe = temptformframe + np.array([len(cubic_nodes),len(cubic_nodes)])
+					tot_cube_frames = np.append(tot_cube_frames,temptformframe,axis=0)
+
+	#print(tot_cube_nodes)
+	#print(tot_cube_frames)
+
+	#The cube is then transformed into the (111) coordinate system
+	#nodes = np.transpose(np.dot(tform111,np.transpose(cube)))
+
+	return hex_nodes,hex_frames,tot_cube_nodes,tot_cube_frames
+
+# Utility for checking 
+# if a given point (or set of points)
+# is contained within a hexagonal volume
+# with a given radius and height
+# 
+def in_hex_volume(points,abs_hex_radius,abs_hex_height):
+	inhexvolume = True
+	factor = np.sqrt(3)/2.0
+	correction = 0.1
+	if all(points.T[2] > abs_hex_height+correction) or all(points.T[2] < -correction):
+		inhexvolume = False
+	if inhexvolume:
+		points = np.transpose(np.dot(init_hex_rot_tform,np.transpose(points)))
+		if all(points.T[0] > factor*abs_hex_radius+correction) or all(points.T[0] < -factor*abs_hex_radius-correction):
+			inhexvolume = False
+		else:
+			points = np.transpose(np.dot(hex_rot_tform,np.transpose(points)))
+			if all(points.T[0] > factor*abs_hex_radius+correction) or all(points.T[0] < -factor*abs_hex_radius-correction):
+				inhexvolume = False
+			else:
+				points = np.transpose(np.dot(hex_rot_tform,np.transpose(points)))
+				if all(points.T[0] > factor*abs_hex_radius+correction) or all(points.T[0] < -factor*abs_hex_radius-correction):
+					inhexvolume = False
+	return inhexvolume
+
+# The cube defined in the cubic coordinate system.
+cube_nodes =  [[0.25,0.25,0.25], #0
+			   [0.25,0.75,0.75], #1
+			   [0.75,0.25,0.75], #2
+			   [0.75,0.75,0.25]] #3
+
+cube_frames = np.array([[0,1],
+					    [0,2],
+					    [1,3],
+					    [2,3]])
+
+hex_nodes, hex_frames, nodes, frames = cubic_to_111(1,1,cube_nodes,cube_frames)
 
 #print(frames,np.shape(nodes))
 #Set up figure plotting
@@ -165,7 +200,7 @@ for i,frame in enumerate(hex_frames):
 		nid2 = int(frame[1])
 		start = [hxs[nid1],hys[nid1],hzs[nid1]]
 		end   = [hxs[nid2],hys[nid2],hzs[nid2]]
-		ax.plot([start[0],end[0]],[start[1],end[1]],[start[2],end[2]],color='b', alpha=0.1)
+		ax.plot([start[0],end[0]],[start[1],end[1]],[start[2],end[2]],color='b', alpha=1.0)
 
 
 #plot the nodes
